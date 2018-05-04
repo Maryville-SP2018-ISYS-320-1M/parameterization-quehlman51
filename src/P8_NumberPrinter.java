93de5ea1a9194ba9a60dcfd5979e72a94e0278a8
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-04 00:34:23 -0500

    Created second method

--- src/P8_NumberPrinter.java
+++ src/P8_NumberPrinter.java
@@ -7,11 +7,19 @@
 public class P8_NumberPrinter {
 
 	public static void main(String[] args) {
-		printNumbers(10);
+		printNumbers(5);
+		System.out.println();
+		System.out.println();
+		printStepRange(12,2);
 
 	}
 		public static void printNumbers(int number) {
 			for(int i = 1; i <= number; i++) {
+				System.out.print("[" + i + "]");
+			}
+		}
+		public static void printStepRange(int range, int step) {
+			for(int i = 0; i <= range; i+= step) {
 				System.out.print("[" + i + "]");
 			}
 		}
